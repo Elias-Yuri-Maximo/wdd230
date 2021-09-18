06de21d3d05b704ee7fbc6b7c1b1f63dc0e1ed80
Elias-Yuri-Maximo <eliasmaximus@gmail.com>
2021-09-18 16:31:46 -0300

    Home assignment pages from Elias Yuri Maximo

---
+++ js/dates.js
@@ -0,0 +1,5 @@
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
+
+const lastupdated = document.getElementById("lastupdated");
+lastupdated.textContent = document.lastModified;
