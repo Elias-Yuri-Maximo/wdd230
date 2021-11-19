3d3ea5f2904e1cf0ab980a91e595242a8ac98de1
Elias-Yuri-Maximo <eliasmaximus@gmail.com>
2021-11-19 15:10:30 -0300

    changes

---
+++ lesson10/assignment-week-10/js/forecast-script.js
@@ -0,0 +1,24 @@
+//const apiURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e5f1135fd924cb47372cbb1fe41a055a";
+const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=69aef0ffa210c9d7c671a4ff748191dd";
+
+
+
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+
+    const sixForecast = jsObject.list.filter( x => x.dt_text.includes('18:00:00'));
+
+    console.log(sixForecast);
+
+       
+       // document.querySelector()
+    //document.getElementById('day'+(i+1)).innerHTML = jsObject.main.temp;
+    
+    //const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    //const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    //document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    //document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    //document.getElementById('icon').setAttribute('alt', desc);
+  });
--- lesson10/weather-api/js/weatherapi.js
+++ lesson10/weather-api/js/weatherapi.js
@@ -1,10 +1,20 @@
 //const apiURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e5f1135fd924cb47372cbb1fe41a055a";
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&appid=69aef0ffa210c9d7c671a4ff748191dd";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=69aef0ffa210c9d7c671a4ff748191dd";
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
   });
+
+
+  //69aef0ffa210c9d7c671a4ff748191dd
 
   //69aef0ffa210c9d7c671a4ff748191dd
