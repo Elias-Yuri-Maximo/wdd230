12544a0617fe73776ca608c99dad9fe26d1b0e79
Elias Maximo <eliasmaximus@gmail.com>
2021-12-15 14:30:58 -0300

    END

--- lesson12/js/home-weather-summary copy.js
+++ lesson12/js/home-weather-summary copy.js
@@ -1,91 +1,13 @@
 
   //const apiURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e5f1135fd924cb47372cbb1fe41a055a";
-  const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=-22.9051&lon=-47.0613&units=imperial&appid=e5f1135fd924cb47372cbb1fe41a055a';
+  const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=3467865&units=imperial&appid=69aef0ffa210c9d7c671a4ff748191dd";
 
   fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
       console.log(jsObject);
-
-
-      let current = jsObject.current;
-      //Puts the temperature 
-      document.querySelector("#temperature").textContent = "Temperature: "+current.temp+"F";
-  
-      //Puts the current condition 
-      document.querySelector("#weather-condition").textContent = "Weather Condition: "+current.weather[0].description;
-  
-      //Puts the Humidity
-      document.querySelector("#humidity").textContent = "Humidity: "+current.humidity+"%";
-
-      //puts the alert on the alert
-      if(jsObject.alerts){
-        document.querySelector('#alerting-institution').textContent=jsObject.alerts[0].sender_name;
-        document.querySelector('#alert').textContent = jsObject.alerts[0].event;
-      }else{
-        document.querySelector('#pancake').display = 'none';
-      }
-
-
-      //Three day forecast
-      ///////////////////////////////////////////////////////////////////////////
-      let forecast = jsObject.daily;
-      console.log(forecast)
-
-
-      //put the dates
-      let today = new Date
-      days = [today.getDay(), today.getDay() + 1, today.getDay()+2 ];
-      //console.log(days)
-      const weekDays = ['Sun','Mon','Tue','Wed','Thu','Fri', 'Sat'];
-
-      let i = 1;
-      for (let day of days){
-        //Sets the day for each box
-        let name = weekDays[day];
-        document.querySelector('#day'+ i).textContent = name ;
-        
-        //Puts the weather icons on the days
-        const imageSrc = 'https://openweathermap.org/img/w/'+forecast[i -1].weather[0].icon +'.png';
-        document.querySelector('#img'+i).setAttribute('src',imageSrc);
-        
-        //Puts the temperature on the forecast
-        const temperature = forecast[i-1].temp.day + ' ºF';
-        document.querySelector('#temperature'+i).innerHTML = temperature
-
-
-        
-        
-        
-        i++;
-      }
-
-
-      /*
-      let i=0;
-      sixForecast.forEach(forecast => {
-        
-       
-        //Gets and prints the days of the weeks
-        let theDate = new Date(forecast.dt_txt);
-
-        if (document.querySelector('#day'+ (i+1))){
-
-        document.querySelector('#day'+ (i+1)).innerHTML = weekDays[theDate.getDay()];
-        document.querySelector('#temperature'+ (i+1)).innerHTML = (forecast.main.temp)+ '°F'; 
-        
-        //Gets and set the icons
-        
-        const imageSrc = 'https://openweathermap.org/img/w/'+forecast.weather[0].icon +'.png';
-        document.querySelector('#img' + (i+1)).setAttribute('src', imageSrc);
-        }
-        i++;
-      });*/
-
-    });
-
      
-      /*Puts the temperature 
+      //Puts the temperature 
       document.querySelector("#temperature").textContent = "Temperature: "+jsObject.main.temp+"F";
   
       //Puts the current condition 
@@ -98,8 +20,14 @@
       
   
   
- 
-
+      //START WIND CHILL CODE 
+  
+      const temp = parseInt(jsObject.main.temp);
+      const windSpeed = parseInt(jsObject.wind.speed);
+  
+      
+      console.log(temp, windSpeed);
+  
   
       
      
@@ -167,8 +95,8 @@
       //document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
       //document.getElementById('icon').setAttribute('alt', desc);
     });
-      }*/
+  
     function closeAlert(){
         document.querySelector(".pancake").style.display = "none";
-    
+
     }
